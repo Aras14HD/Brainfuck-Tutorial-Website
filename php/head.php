bdf19484126a538b2835be09ca6d5bb1eb9f133e
Aras14HD <jorimbk@gmail.com>
2020-07-27 13:20:43 +0200

    -added Jquery

--- php/head.php
+++ php/head.php
@@ -1,4 +1,5 @@
 <meta charset="utf-8">
 <link rel="stylesheet" href="../css/main.css">
 <link rel="icon" type="image/png" href="../text34.png">
+<script type="text/javascript" src="../js/jquery-3.5.1.min.js"></script>
 <meta name="description" content="Eine Einführung in die Programmiersprache Brainfuck.">
